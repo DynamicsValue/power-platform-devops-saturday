ba5d4fc56fe898f789617d996d044b59e2ab5cd2
Jordi <jordi.montana@gmail.com>
2020-06-20 06:27:32 -0700

    Adding sample code and slides

---
+++ DevOpsSat.Plugins.UnitTests/PhoneCallCreatePluginTests.cs
@@ -0,0 +1,63 @@
+﻿using FakeXrmEasy;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Linq;
+using TypedEntities;
+using Xunit;
+
+namespace DevOpsSat.Plugins.UnitTests
+{
+    public class PhoneCallCreatePluginTests
+    {
+        private readonly Contact _contact;
+        private readonly PhoneCall _phoneCall;
+        private readonly IOrganizationService _service;
+        private readonly XrmFakedContext _context;
+
+        public PhoneCallCreatePluginTests()
+        {
+            _context = new XrmFakedContext();
+            _service = _context.GetOrganizationService();
+
+            _contact = new Contact() { Id = Guid.NewGuid() };
+
+            _phoneCall = new PhoneCall()
+            {
+                Id = Guid.NewGuid(),
+                PhoneNumber = "3650000",
+                RegardingObjectId = _contact.ToEntityReference()
+            };
+        }
+
+        [Fact]
+        public void Should_create_phonecall_history_record_for_contact_and_phonenumber()
+        {
+            _context.ExecutePluginWithTarget<PhoneCallCreatePlugin>(_phoneCall);
+
+            var phoneCallHistory = _context.CreateQuery<ultra_phonecallhistory>().FirstOrDefault();
+            Assert.NotNull(phoneCallHistory);
+
+            Assert.Equal(_contact.Id, phoneCallHistory.ultra_contactid.Id);
+            Assert.Equal(_phoneCall.PhoneNumber, phoneCallHistory.ultra_phonenumber);
+
+        }
+
+        [Fact]
+        public void Should_not_create_phonecall_history_record_for_the_same_phonenumber_and_contact_pair()
+        {
+            var existingPhoneCallHistoryRecord = new ultra_phonecallhistory()
+            {
+                Id = Guid.NewGuid(),
+                ultra_contactid = _contact.ToEntityReference(),
+                ultra_phonenumber = _phoneCall.PhoneNumber
+            };
+
+            _context.Initialize(existingPhoneCallHistoryRecord);
+
+            _context.ExecutePluginWithTarget<PhoneCallCreatePlugin>(_phoneCall);
+
+            var phoneCallHistoryList = _context.CreateQuery<ultra_phonecallhistory>().ToList();
+            Assert.Single(phoneCallHistoryList);
+        }
+    }
+}
---
+++ DevOpsSat.Plugins/DevOpsSat.Plugins/PhoneCallCreatePlugin.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using TypedEntities;
+using System.Linq;
+
+namespace DevOpsSat.Plugins
+{
+    public class PhoneCallCreatePlugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            #region Boilerplate
+            // Obtain the execution context from the service provider.
+            IPluginExecutionContext context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            // Obtain the organization service reference.
+            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+            #endregion
+
+            if(context.InputParameters.Contains("Target") 
+                && context.InputParameters["Target"] is Entity)
+            {
+                var phoneCall = (context.InputParameters["Target"] as Entity).ToEntity<PhoneCall>();
+
+                using(var ctx = new XrmServiceContext(service))
+                {
+                    var existingPhoneCallHistoryRecord = (from ph in ctx.CreateQuery<ultra_phonecallhistory>()
+                                                          where ph.ultra_contactid.Id == phoneCall.RegardingObjectId.Id
+                                                          where ph.ultra_phonenumber == phoneCall.PhoneNumber
+                                                          select ph).FirstOrDefault();
+
+                    if(existingPhoneCallHistoryRecord == null)
+                    {
+                        var phoneCallHistory = new ultra_phonecallhistory()
+                        {
+                            ultra_contactid = phoneCall.RegardingObjectId,
+                            ultra_phonenumber = phoneCall.PhoneNumber
+                        };
+
+                        service.Create(phoneCallHistory);
+                    }
+                }
+
+                
+            }
+
+                          
+        }
+    }
+}
